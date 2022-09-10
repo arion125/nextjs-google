e830dfd23bb893461be4ba32a9cab0b06670be6e
arion125 <meta.arion@proton.me>
2022-09-10 22:25:58 +0200

    create header

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import User from "./User";
+
+const Header = () => {
+  return (
+    <header className="h-16 text-gray-700 flex justify-between pr-4 pl-4 items-center text-sm font-normal">
+      <div className="flex gap-4">
+        <p className="link">About</p>
+        <p className="link">Store</p>
+      </div>
+      <div className="flex gap-4">
+        <p className="link">Gmail</p>
+        <p className="link">Images</p>
+        <User />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
